e532ce26b0ce13e1446727baf6ad2d5292c73fed
Diego Sebastian Markiewicz <dieghard@gmail.com>
2024-03-01 17:29:35 -0300

    ```

---
+++ src/utils/dataUtil.jsx
@@ -0,0 +1,58 @@
+export const data = [
+	{
+		id: 1,
+		title: "HE MAN",
+		slug: "1972",
+		subtitle: "POR EL PODER DE TU HERMANA",
+		description: "Francis Ford Coppola",
+		content: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
+		poster:
+			"https://img.cronista.com/files/image/298/298825/5ffe0df501931_950_534!.jpg?s=3588dd461fb33ee5a19304e1f3437c53&d=1708420408"
+	},
+	{
+		id: 2,
+		title: "The Godfather II",
+		slug: "1972",
+		subtitle: "The Godfather",
+		description: "Francis Ford Coppola",
+		content: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
+		poster:
+			"https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+	},
+	{
+		id: 3,
+		title: "The Godfather",
+		slug: "1972",
+		subtitle: "The Godfather",
+		description: "Francis Ford Coppola",
+		content: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
+		poster: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNm"
+	},
+	{
+		id: 3,
+		title: "The Godfather",
+		slug: "1972",
+		subtitle: "The Godfather",
+		description: "Francis Ford Coppola",
+		content: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
+		poster: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNm"
+	},
+	{
+		id: 3,
+		title: "The Godfather",
+		slug: "1972",
+		subtitle: "The Godfather",
+		description: "Francis Ford Coppola",
+		content: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
+		poster: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNm"
+	},
+	{
+		id: 3,
+		title: "The Godfather",
+		slug: "1972",
+		subtitle: "The Godfather",
+		description: "Francis Ford Coppola",
+		content: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
+		poster: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNm"
+	}
+];
